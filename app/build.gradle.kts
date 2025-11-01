557325bf593b1a2d7eb3ffdd78bc85b8929633b1
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-11-01 21:54:03 +0200

    update Java version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -9,6 +11,7 @@ android {
     compileSdk {
         version = release(36)
     }
+    buildToolsVersion = "36.0.0"
 
     defaultConfig {
         applicationId = "com.nicos.material3expressivelist"
@@ -30,11 +33,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.fromTarget("21")
+        }
     }
     buildFeatures {
         compose = true
@@ -43,13 +48,16 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
+    // Compose
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    // Material Design 3
     implementation(libs.androidx.compose.material3)
+    // Unit Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
