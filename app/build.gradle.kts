096b887ef2ebe697abb459c2b4353df03c899cf2
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-05-30 01:19:15 +0300

    set Target SDK to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,14 +38,17 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget = JvmTarget.fromTarget("21")
-        }
-    }
     buildFeatures {
         compose = true
         buildConfig = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_3
+        jvmTarget = JvmTarget.fromTarget("21")
+        freeCompilerArgs = listOf("-Xannotation-default-target=param-property")
     }
 }
 
